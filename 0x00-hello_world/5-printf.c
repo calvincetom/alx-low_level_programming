e4f54e02a6405523c957692354e7c4fe49ec4e73
calvince <calvincetom@outlook.com>
2022-03-10 11:02:26 -0800

    remove trailing white space at the beginning of each line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar, 
+ * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
- * return: 0 if exited properly, no-zero otherwise
+ * Return: 0 if exited properly, no-zero otherwise
  */
 int main(void)
 {
