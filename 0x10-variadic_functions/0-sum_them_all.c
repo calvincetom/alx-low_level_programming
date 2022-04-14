38ce8a37f060795656f61c33547c59799cfbd061
calvincetom <calvincetom@outlook.com>
2022-04-15 00:55:21 +0300

    correct betty error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,11 @@
 #include "variadic_functions.h"
 
 /**
- *  * sum_them_all - returns the sum of all its parameters.
- *   * @n: amount of the arguments.
- *    *
- *     * Return: sum of its parameters.
- *      */
+ * sum_them_all -returns the sum of all its parameters.
+ * @n:amount of the arguments.
+ *
+ * Return: sum of its parameters.
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
